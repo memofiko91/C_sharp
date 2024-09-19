2518b15a84c7619e1eded9a6157672fb8be423c9
s54823 <s54823@CE-114>
2024-09-19 10:00:30 +0200

    Add project files.

---
+++ MyFirstConsoleApp/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace MyFirstConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("This is my first program.");
+        }
+    }
+}
