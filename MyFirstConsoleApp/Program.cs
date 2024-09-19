90b3d4ce623a7461d96c8630d55fc9393f65fda1
Kolarczyk Melissa <s54823@edu.campus02.at>
2024-09-19 12:51:09 +0200

    added division

--- MyFirstConsoleApp/Program.cs
+++ MyFirstConsoleApp/Program.cs
@@ -1,6 +1,8 @@
 ﻿namespace MyFirstConsoleApp
 {
     // format code: strg + k + d
+    // mehrere Zeilen auskommentieren: strg + k + c (rückgängig mit strg+k+u)
+    // 
 
     // $ vor de String ermöglicht das Einbauen von Variablen mit {}
 
@@ -15,7 +17,7 @@
             Console.WriteLine("Hallo, wie heißt du?");
             String name = Console.ReadLine();
             Console.WriteLine("Wie alt bist du?");
-            String age = Console.ReadLine();    
+            String age = Console.ReadLine();
             Console.WriteLine($"Dein Name ist {name} und du bist {age} Jahre alt!");
 
             // mit Funktion Convert.ToInt32 (--> um mit variable zu rechnen)
@@ -24,6 +26,11 @@
             int myage = 33;
             Console.WriteLine($"Du heißt {name} und zusammen sind wir {myage + yourage} Jahre alt!");
 
+            // Division zweier Zahlen
+            int a = 12;
+            int b = 7;
+            double erg = (double) a / b;  // muss gecastet werden, damit Nachkommazahlen angezeigt werden
+            Console.WriteLine(erg);
 
         }
     }
